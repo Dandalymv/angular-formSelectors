7ebc80cae8d19a83fd5ca98246023b75ebe31b5a
Dandaly Morales <dan.moralesv@gmail.com>
2022-06-16 20:32:11 -0400

    first commit

---
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
+import { PaisSmall } from '../../interfaces/paises.interface';
+import { switchMap, tap } from 'rxjs';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent implements OnInit {
+
+  miFormulario: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    pais: ['', Validators.required],
+    frontera: ['', Validators.required]
+  })
+
+  //llenar selectores
+  regiones: string[] = [];
+  paises: PaisSmall[] = [];
+  // fronteras: string[] = [];
+  fronteras: PaisSmall[] = [];
+  
+  //UI
+  cargando: boolean = false;
+
+  constructor( 
+    private fb: FormBuilder,
+    private paisesService: PaisesService
+    ) { }
+
+  ngOnInit(): void {
+    this.regiones = this.paisesService.regiones
+
+
+    //Cuando cambia la Región
+    this.miFormulario.get('region')?.valueChanges
+      .pipe(
+        tap( ( _ ) => {
+          this.miFormulario.get('pais')?.reset('');
+          this.cargando = true;
+        }),   
+        switchMap(region => this.paisesService.getPaisesPorRegion(region) )   
+      )
+      .subscribe( paises => {
+        this.paises = paises;
+        this.cargando = false;
+      })
+
+
+    //Cuando cambia el país
+    this.miFormulario.get('pais')?.valueChanges
+    .pipe(
+      tap(() => {
+        this.miFormulario.get('frontera')?.reset('');
+        this.cargando = true;
+      }),
+      switchMap( codigo => this.paisesService.getPaisPorCodigo( codigo )),
+      switchMap( pais => this.paisesService.getPaisesPorCodigos(pais?.borders!) )
+    )
+    .subscribe(paises => {
+        //this.fronteras = pais?.borders || [];
+        this.fronteras = paises;
+        console.log(paises)
+        this.cargando = false;
+    })
+  }
+
+  guardar(){
+    console.log(this.miFormulario.value)
+  }
+
+}
